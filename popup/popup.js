cab7d77765da67ef4f4d6729b616e691c4d5185a
yhotta240 <yhotta240@icloud.com>
2024-10-17 14:00:59 +0900

    update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,6 @@
 
 // イベントリスナーを設定
 document.getElementById('insertMeetStart').addEventListener('click', () => insertTag('{meet開始時刻}'));
-document.getElementById('insertMeetEnd').addEventListener('click', () => insertTag('{meet終了時刻}'));
 document.getElementById('insertCaptionStart').addEventListener('click', () => insertTag('{字幕ログ開始時刻}'));
 document.getElementById('insertCaptionEnd').addEventListener('click', () => insertTag('{字幕ログ終了時刻}'));
 // タグを挿入する関数
@@ -19,27 +18,25 @@ function insertTag(tag) {
   textarea.focus(); // フォーカスをテキストエリアに戻す
 }
 
+const defaultHeaderText =
+  `-----------------------------------------------\n` +
+  `{meet開始時刻}\n` +
+  `{字幕ログ開始時刻} - {字幕ログ終了時刻}\n` +
+  `打ち合わせ\n` +
+  `-----------------------------------------------\n\n`;
 
 // 設定をリセットする関数
 function resetSettings() {
-  const defaultHeaderText =
-    `-----------------------------------------------\n` +
-    `{meet開始時刻}\n` +
-    `{字幕ログ開始時刻} - {字幕ログ終了時刻}\n` +
-    `打ち合わせ\n` +
-    `-----------------------------------------------\n\n`;
-
   document.getElementById('fileName').value = 'caption'; // ファイル名をリセット
   document.getElementById('fileFormat').value = 'txt'; // ファイル名をリセット
   document.getElementById('headerText').value = defaultHeaderText; // テキストエリアをデフォルトにリセット
-  saveSettings(); // リセットされた設定を保存
-  console.log('設定がリセットされました');
+  saveSettings(dateTime(), '設定がリセットされました'); // リセットされた設定を保存
 }
 document.getElementById('resetButton').addEventListener('click', () => resetSettings());
 
 
 // 設定を保存する関数
-function saveSettings() {
+function saveSettings(datetime, message) {
   const fileName = document.getElementById('fileName').value; // ファイル名を取得
   const fileFormat = document.getElementById('fileFormat').value; // ファイル形式を取得
   const headerText = document.getElementById('headerText').value; // ヘッダーのテキストを取得
@@ -49,26 +46,52 @@ function saveSettings() {
     fileName: fileName,
     fileFormat: fileFormat,
     headerText: headerText,
+    message: [datetime, message],
   };
 
   // chrome.storage.localに保存
   chrome.storage.local.set({ settings: settings }, () => {
     console.log('設定が保存されました:', settings);
+    logOutput(datetime, message);
   });
 }
-document.getElementById('saveButton').addEventListener('click', () => {
-  // saveCaptionsToFile(); // ファイルを保存する関数を呼び出す
-  saveSettings(); // 設定を保存する関数を呼び出す
+document.getElementById('saveButton').addEventListener('click', () => { 
+  saveSettings(dateTime(), '設定が保存されました'); 
 });
 
+
+
+function logOutput(datetime, message) {
+  const logDiv = document.getElementById('log');
+  logDiv.innerHTML += '<p class="m-0">' + datetime + ': ' + message + '</p>'; // <p> タグで囲んで新しい行にする
+}
+
+function dateTime() {
+  // 現在の日付と時刻を取得
+  const now = new Date();
+  const year = now.getFullYear();           // 年
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // 月
+  const day = String(now.getDate()).padStart(2, '0');         // 日
+  const hours = String(now.getHours()).padStart(2, '0');       // 時
+  const minutes = String(now.getMinutes()).padStart(2, '0');   // 分
+  const seconds = String(now.getSeconds()).padStart(2, '0');   // 秒
+  const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  console.log(formattedDateTime);
+  return formattedDateTime;
+}
 
 // 保存された設定を読み込む関数
 function loadSettings() {
   chrome.storage.local.get('settings', (data) => {
     if (data.settings) {
+
       document.getElementById('fileName').value = data.settings.fileName || 'captions'; // 修正
       document.getElementById('fileFormat').value = data.settings.fileFormat || 'text/plain'; // 修正
-      document.getElementById('headerText').value = data.settings.headerText || ''; // ヘッダーのテキストを設定
+      document.getElementById('headerText').value = data.settings.headerText || defaultHeaderText; // ヘッダーのテキストを設定
+      const logDiv = document.getElementById('log');
+      logDiv.innerHTML =  '<p class="m-0">' + data.settings.message[0] + ': ' + data.settings.message[1] + '</p>'; // 保存されたログを表示
+      
+      console.log( data.settings.message);
     }
   });
 }
